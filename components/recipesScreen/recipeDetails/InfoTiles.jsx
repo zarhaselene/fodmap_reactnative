afa28828221ba19dedf8e61c01b0dc768308a57c
Jan <kupper.thorge@gmail.com>
2025-06-09 15:48:58 +0200

    feat: refactor InfoTiles component to improve icon structure and enhance visual consistency

--- components/recipesScreen/recipeDetails/InfoTiles.jsx
+++ components/recipesScreen/recipeDetails/InfoTiles.jsx
@@ -14,18 +14,22 @@ export default function InfoTiles({ recipe }) {
     <View className='flex-row w-full gap-4 mt-4'>
       {/* Prep time  */}
       <View className='flex-1 items-center bg-surface-secondary rounded-lg p-4'>
-        <MaterialCommunityIcons name='clock-outline' color={'#14b8a6'} size={20} />
+        <Text className='text-primary'>
+          <MaterialCommunityIcons name='clock-outline' size={20} />
+        </Text>
         <Text className='text-text-muted text-sm'>Prep</Text>
         <Text className='font-semibold'>{recipe.prep_time}m</Text>
       </View>
       {/* Cook time  */}
       <View className='flex-1 items-center bg-surface-secondary rounded-lg p-4'>
-        <MaterialCommunityIcons name='alarm' color={'#14b8a6'} size={20} />
+        <Text className='text-primary'>
+          <MaterialCommunityIcons name='alarm' size={20} />
+        </Text>
         <Text className='text-text-muted text-sm'>Cook</Text>
         <Text className='font-semibold'>{recipe.cook_time}m</Text>
       </View>
       {/* Servings  */}
-      <View className='flex-1 items-center bg-surface-secondary rounded-lg p-4'>
+      {/* <View className='flex-1 items-center bg-surface-secondary rounded-lg p-4'>
         <MaterialCommunityIcons name='account-multiple-outline' color={'#14b8a6'} size={20} />
         <Text className='text-text-muted text-sm'>Serves</Text>
         <View className='flex-row items-center gap-2'>
@@ -37,11 +41,22 @@ export default function InfoTiles({ recipe }) {
             <MaterialCommunityIcons name='plus' size={14} color={'#14b8a6'} />
           </TouchableOpacity>
         </View>
+      </View> */}
+      {/* Fodmap level  */}
+      <View className='flex-1 items-center bg-surface-secondary rounded-lg p-4'>
+        <Text className='text-primary'>
+          <MaterialCommunityIcons name='chart-arc' size={20} />
+        </Text>
+        <Text className='text-text-muted text-sm'>Fodmap</Text>
+        <Text className='font-semibold'>{recipe.level.split(' ')[0]}</Text>
       </View>
       {/* Calories  */}
       <View className='flex-1 items-center bg-surface-secondary rounded-lg p-4'>
-        <Text className='font-bold text-primary pt-1'>{recipe.calories}</Text>
+        <Text className='text-primary'>
+          <MaterialCommunityIcons name='fire' size={20} />
+        </Text>
         <Text className='text-text-muted text-sm'>Calories</Text>
+        <Text className='font-semibold'>{recipe.calories}</Text>
       </View>
     </View>
   );
